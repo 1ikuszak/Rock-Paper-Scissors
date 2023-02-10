b818a39083e42de528978d726d59605e9e289f28
1ikuszak <luke.glica@gmail.com>
2023-02-10 17:18:30 +0100

    rand num gen and computer function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello")
+function RandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.ceil(max)
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+
+function getComputerChoice(num) {
+    if (num === 1) {
+        return 'rock'
+    } 
+    else if (num === 2) {
+        return 'paper'
+    } 
+    else if (num === 3) {
+        return 'scissors'
+    }
+}
+
+
+let random_number = RandomInt(1,3)
+console.log(getComputerChoice(random_number))
