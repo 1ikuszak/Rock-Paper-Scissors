10f5ba9527d6970b27c863904119af9f0fa6fc3f
1ikuszak <luke.glica@gmail.com>
2023-02-26 17:03:41 +0100

    added event listener to all buttons

--- projectile.js
+++ projectile.js
@@ -1,25 +1,27 @@
 const projectile_tube = document.getElementById("projectile_tube");
 const projectileContainer = document.getElementById("projectileContainer");
-const shootButton = document.getElementById("shootButton");
+const shootButtons = document.querySelectorAll("#shootButton");
 
-shootButton.addEventListener("click", function() {
 
-  //player
-  const player_projectile = document.createElement("div");
-  player_projectile.classList.add("projectile");
-  player_projectile.classList.add("player");
-  projectile_tube.appendChild(player_projectile);  
-  player_projectile.addEventListener("animationend", function() {
-    player_projectile.remove();
+shootButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    //player
+    const player_projectile = document.createElement("div");
+    player_projectile.classList.add("projectile");
+    player_projectile.classList.add("player");
+    projectile_tube.appendChild(player_projectile);  
+    player_projectile.addEventListener("animationend", function() {
+      player_projectile.remove();
+    });
+
+    //enemy
+    const enemy_projectile = document.createElement("div");
+    enemy_projectile.classList.add("projectile");
+    enemy_projectile.classList.add("enemy");
+    projectile_tube.appendChild(enemy_projectile);  
+    enemy_projectile.addEventListener("animationend", function() {
+      enemy_projectile.remove();
+    });
   });
-
-  //enemy
-  const enemy_projectile = document.createElement("div");
-  enemy_projectile.classList.add("projectile");
-  enemy_projectile.classList.add("enemy");
-  projectile_tube.appendChild(enemy_projectile);  
-  enemy_projectile.addEventListener("animationend", function() {
-    enemy_projectile.remove();
-  });
-  
 });
+
